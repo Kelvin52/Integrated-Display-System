c4b89bb0e1df5aea5e56f8c4241119b6e75dfea1
Kelvin52 <37885095+Kelvin52@users.noreply.github.com>
2019-04-06 14:04:16 +1300

    Clearn up code structure & Nav bar on top

--- DPS/drive-download-20190304T234130Z-001/script.js
+++ DPS/drive-download-20190304T234130Z-001/script.js
@@ -1,21 +1,21 @@
-function doDate()
-{
-var str = "";
 
+function doDate(){
+
+    var str = "";
     var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
     var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-
+    var seconds;
     var now = new Date();
 
-    str += "Today is: " + days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+    str += days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes(); // + ":" + now.getSeconds();
 
 
-  	document.getElementById("time").innerHTML =  str ;
- }
+    document.getElementById("time").innerHTML =  str ;
+}
 	
-	setInterval(doDate, 1000);
+setInterval(doDate, 1000);
 
-	document.getElementById('hnsButt').onclick = "location.href = 'http://127.0.0.1:8080/health%20And%20Safety%20Page.html'";
+document.getElementById('hnsButt').onclick = "location.href = 'http://127.0.0.1:8080/health%20And%20Safety%20Page.html'";
 	
-	document.getElementById('TTbutt').onclick = "location.href = 'http://127.0.0.1:8080/Prototype2.html'";
+document.getElementById('TTbutt').onclick = "location.href = 'http://127.0.0.1:8080/Prototype2.html'";
 	
