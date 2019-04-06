0b08b07621e229c51cff9e37c9269403278fa02a
Kelvin52 <37885095+Kelvin52@users.noreply.github.com>
2019-04-07 01:03:30 +1300

    Same as last commit

--- DPS/drive-download-20190304T234130Z-001/script.js
+++ DPS/drive-download-20190304T234130Z-001/script.js
@@ -1,5 +1,35 @@
 
-function doDate(){
+ function doDate(){
+
+  var str = "";
+
+  var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+  var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+  var now = new Date();
+
+  str = days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+
+
+  document.getElementById("time").innerHTML =  str ;
+ }
+setInterval(doDate, 1000);
+
+var timeouttime = 1000;
+var timeout = setTimeout(function() {
+  window.location.href = "Promo_Vid.html";
+}, timeouttime);
+
+function ResetRedir(){
+  console.log("mouse clicked");
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    window.location.href = "Promo_Vid.html";
+  }, timeouttime);
+}
+
+//window.addEventListener("click",ResetRedir); alt if onmousedown doesn't work
+  
+ /* function doDate(){
 
     var str = "";
     var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
@@ -10,12 +40,12 @@ function doDate(){
     str += days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes(); // + ":" + now.getSeconds();
 
 
-    document.getElementById("clock").innerHTML =  str ;
+    document.getElementById("time").innerHTML =  str ;
 }
 	
-setInterval(doDate, 1000);
+setInterval(doDate, 1000); */
 
-document.getElementById('hnsButt').onclick = "location.href = 'http://127.0.0.1:8080/health%20And%20Safety%20Page.html'";
+//document.getElementById('hnsButt').onclick = "location.href = 'http://127.0.0.1:8080/health%20And%20Safety%20Page.html'";
 	
-document.getElementById('TTbutt').onclick = "location.href = 'http://127.0.0.1:8080/Prototype2.html'";
+//document.getElementById('TTbutt').onclick = "location.href = 'http://127.0.0.1:8080/Prototype2.html'";
 	
